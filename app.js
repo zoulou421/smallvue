3a9ec8d14999d10715e4bf497e447635a28537c1
Bonevy BEBY <bonevybeby@gmail.com>
2024-03-15 06:02:46 +0100

    added: created first component

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+
+})
+.component('click-counter',{
+    template:'<button @click="count++">{{count}}</button>',
+    data() {
+        return {
+            count:0
+        }
+    },
+
+})
+.mount('#app')
