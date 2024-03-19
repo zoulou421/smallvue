64591d735cc99ccf626b4c507a22536e2cf83cb5
Bonevy BEBY <bonevybeby@gmail.com>
2024-03-19 14:20:49 +0100

    added: local child component in global parent component

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+let PlanComponent={
+
+    template:'#plan-template',
+    props:{
+        name: {type:String,required:true},
+        price: Number
+    }
+}
+
+
 const app = Vue.createApp({
+   
+})
+
+.component('plan-picker',{
+    components:{
+        plan:PlanComponent
+    },
+    template:'#plan-picker-template',
     data() {
         return {
             plans:['The Single','The Curious','The Addict']
         }
     },
-})
-.component('plan',{
-    template:'#plan-template',
-    props:{
-        name: {type:String, default:"Default Value",required:true},
-        price: Number
-    }
+   
 })
 .mount('#app')
