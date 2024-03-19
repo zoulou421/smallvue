1be2409a877f1b82c237b025bab114494eec8378
Bonevy BEBY <bonevybeby@gmail.com>
2024-03-19 12:21:14 +0100

    added: x-template component

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const app = Vue.createApp({
 
 })
-.component('click-counter',{
-    template:'<button @click="count++">{{count}}</button>',
+
+.component('click-counter1',{
+    template:`<button @click="count++">{{count}}</button>`,
+    data() {
+        return {
+            count:0
+        }
+    },
+
+})
+.component('click-count',{
+    template:'#click-count-template',
     data() {
         return {
             count:0
